8d8adfdd67de3757718f7e8d885efadd7803b6a0
John Albright <jalbright10@gmail.com>
2018-01-10 23:35:17 -0500

    Add IDungeon interface

--- MetaZelda/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MetaZelda
-{
-    public class Class1
-    {
-    }
-}
---
+++ MetaZelda/IDungeon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaZelda
+{
+    /// <summary>
+    /// Represents the spacial layout of a lock-and-key puzzle and contains all
+    /// <see cref="Symbol"/>s, <see cref="Room"/>s, and <see cref="Edge"/>s within
+    /// the puzzle.
+    /// </summary>
+    interface IDungeon
+    {
+        ICollection<Room> GetRooms();
+        int RoomCount();
+        Room Get(int id);
+        void Add(Room room);
+        void LinkOneWay(Room room1, Room room2);
+        void Link(Room room1, Room room2);
+        void LinkOneWay(Room room1, Room room2, Symbol cond);
+        void Link(Room room1, Room room2, Symbol cond);
+        bool RoomsAreLinked(Room room1, Room room2);
+        Room FindStart();
+        Room FindBoss();
+        Room FindGoal();
+        Room FindSwitch();
+        Rect2I GetExtentBounds();
+    }
+}
