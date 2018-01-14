2b018f8ae785deef8d3a91441a47dcb0f51ea036
John Albright <jalbright10@gmail.com>
2018-01-14 16:58:54 -0500

    Add Symbol and Condition classes

---
+++ MetaZelda/Condition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaZelda
+{
+    /// <summary>
+    /// Used to represent <see cref="Room"/>s' preconditions.
+    /// <para>
+    /// A Room's precondition can be considered the set of Symbols from the other
+    /// Rooms that the player must have collected to be able to reach this room. For
+    /// instance, if the Room is behind a locked door, the precondition for the
+    /// Room includes the key for that lock.
+    /// </para>
+    /// <para>
+    /// In practice, since there is always a time ordering on the collection of keys,
+    /// this can be implemented as a count of the number of keys the player must have
+    /// (the 'keyLevel').
+    /// </para>
+    /// <para>
+    /// The state of the <see cref="Dungeon"/>'s switch is also recorded in the Condition.
+    /// A Room behind a link that requires the switch to be flipped into a particular
+    /// state will have a precondition that includes the switch's state.
+    /// </para>
+    /// <para>
+    /// A Condition is 'satisfied' when the player has all the keys it requires and
+    /// when the dungeon's switch is in the state that it requires.
+    /// </para>
+    /// <para>
+    /// A Condition x implies a Condition y if and only if y is satisfied whenever x is.
+    /// </para>
+    /// </summary>
+    public class Condition
+    {
+        // NOTE: can't use 'static' keyword here, make sure that SwitchState can be referrenced without instantiating the class
+        /// <summary>
+        /// A type to represent the required state of a switch for the Condition to
+        /// be satisfied.
+        /// </summary>
+        public enum SwitchState
+        {
+            /// <summary>
+            /// The switch may be in any state.
+            /// </summary>
+            Either,
+
+            /// <summary>
+            /// The switch must be off.
+            /// </summary>
+            Off,
+
+            /// <summary>
+            /// The switch must be on.
+            /// </summary>
+            On
+        };
+
+        public Symbol ToSymbol()
+        {
+
+        }
+    }
+}
---
+++ MetaZelda/Symbol.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaZelda
+{
+    /// <summary>
+    /// Represents a single key or lock within the lock-and-key puzzle.
+    /// <para>
+    /// Each Symbol has a 'value'. Two Symbols are equivalent if they have the same
+    /// 'value'.
+    /// </para>
+    /// </summary>
+    public class Symbol
+    {
+        /// <summary>
+        /// Symbol map with special meanings.
+        /// <para>
+        /// Certain items (such as START, GOAL, BOSS) serve no purpose in the puzzle
+        /// other than as markers for the client of the library to place special game
+        /// objects.
+        /// </para>
+        /// <para>
+        /// The SWITCH_ON and SWITCH_OFF symbols do not appear in rooms, only in
+        /// <see cref="Condition"/>s and <see cref="Edge"/>s.
+        /// </para>
+        /// </summary>
+        public const int
+            START = -1,
+            GOAL = -2,
+            BOSS = -3,
+            SWITCH_ON = -4,     // used as a condition (lock)
+            SWITCH_OFF = -5,    // used as a condition (lock)
+            SWITCH = -6;        // used as an item (key) within a room
+
+        protected readonly int value;
+        protected readonly String name;
+
+        /// <summary>
+        /// Creates a Symbol with the given value.
+        /// </summary>
+        /// <param name="value">Value to give the Symbol.</param>
+        public Symbol(int value)
+        {
+            this.value = value;
+
+            if (value == START)
+                name = "Start";
+            else if (value == GOAL)
+                name = "Goal";
+            else if (value == BOSS)
+                name = "Boss";
+            else if (value == SWITCH_ON)
+                name = "ON";
+            else if (value == SWITCH_OFF)
+                name = "OFF";
+            else if (value == SWITCH)
+                name = "SW";
+            else if (value >= 0 && value < 26)
+                name = Char.ToString((char)((int)'A' + value));
+            else
+                name = value.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                return value == ((Symbol)obj).value;
+            }
+        }
+
+        public static bool Equals(Symbol a, Symbol b)
+        {
+            if (a == b) return true;
+            if (b == null) return a.Equals(b);
+            return b.Equals(a);
+        }
+
+        public override int GetHashCode()
+        {
+            return value;
+        }
+        
+        /// <returns>The value of the Symbol.</returns>
+        public int GetValue()
+        {
+            return value;
+        }
+        
+        /// <returns>Whether the Symbol is the special START symbol.</returns>
+        public bool IsStart()
+        {
+            return value == START;
+        }
+        
+        /// <returns>Whether the Symbol is the special GOAL symbol.</returns>
+        public bool IsGoal()
+        {
+            return value == GOAL;
+        }
+        
+        /// <returns>Whether the Symbol is the special BOSS symbol.</returns>
+        public bool IsBoss()
+        {
+            return value == BOSS;
+        }
+        
+        /// <returns>Whether the Symbol is the speciall SWITCH symbol.</returns>
+        public bool IsSwitch()
+        {
+            return value == SWITCH;
+        }
+        
+        /// <returns>Whether the Symbol is one of the special SWITCH_{ON, OFF} symbols.</returns>
+        public bool IsSwitchState()
+        {
+            return value == SWITCH_ON || value == SWITCH_OFF;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
